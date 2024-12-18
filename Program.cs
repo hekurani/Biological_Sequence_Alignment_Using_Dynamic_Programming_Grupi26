137c1cda75e47d602c7d9626e7de3a389e2f141c
Albiona Mustafa <97847891+albionamustafa@users.noreply.github.com>
2024-12-18 19:52:30 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DNA_Sequence_Alignment_Using_Dynamic_Programming_
+{
+    static class Program
+    {
+      
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
